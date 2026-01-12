81002701b89f3dd78b4776c5fa00e3d4cbcf588c
Brent Anderson <bma42@students.uwf.edu>
2026-01-11 18:38:15 -0600

    using python-docx to simplifity validation

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module det014.memorandum_autograder {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens det014.memorandum_autograder to javafx.fxml;
-    exports det014.memorandum_autograder;
-}
